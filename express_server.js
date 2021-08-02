7b759fd1dc1d3bba15ad7adcff05d399176c041c
Dianne <mdianne.gabriel@gmail.com>
2021-08-02 15:24:15 -0400

    Implemented generateRandomString function to use it to shorten the URLs

--- express_server.js
+++ express_server.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+const charSelection = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+app.use(bodyParser.urlencoded({extended: true}));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+
+function generateRandomString(chars) {
+  let result = "";
+  for (let i = 6; i > 0; i--) {
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return result;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -19,9 +30,19 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  // res.send(generateRandomString(charSelection));
 });
 
 app.listen(PORT, () => {
