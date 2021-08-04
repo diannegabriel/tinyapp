a8392708b5fcb198864bc5c2443d1ea118868cec
Dianne <mdianne.gabriel@gmail.com>
2021-08-04 14:02:09 -0400

    Modified /urls/new so only logged in users can shorten links

--- express_server.js
+++ express_server.js
@@ -69,11 +69,16 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { 
-    urls: urlDatabase, 
-    user: users[req.cookies["user_id"]]
-  };
-  res.render("urls_new", templateVars);
+  if (req.cookies["user_id"]) {
+    const templateVars = { 
+      urls: urlDatabase, 
+      user: users[req.cookies["user_id"]]
+    };
+    res.render("urls_new", templateVars);
+  } else {
+    res.redirect("/login")
+  }
+
 });
 
 app.get("/urls/:shortURL", (req, res) => {
